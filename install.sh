ef06502b32e2a0ad4c9ed417a438d8dd73dbe5d5
yejinlei <4010896@qq.com>
2018-12-05 16:39:10 +0800

    auto install

---
+++ install.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/yejinlei/ppa/master/add-apt-repository.sh
+cp add-apt-repository.sh /usr/sbin/add-apt-repository
+chmod o+x /usr/sbin/add-apt-repository
+chown root:root /usr/sbin/add-apt-repository
